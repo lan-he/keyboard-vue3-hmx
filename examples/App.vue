1e50b1d02d0718568f1cac10ba4dd0105d9f2f79
Lan <15669147150g@gmail.com>
2024-10-31 19:22:21 +0800

    完善代码

--- examples/App.vue
+++ examples/App.vue
@@ -4,12 +4,18 @@ import Keyboard from '../packages/keyboard/src/keyboard.vue'
 import FalseTextbox from '../packages/false-textbox/src/false-textbox.vue'
 const msgs = ref('')
 const inputKey = (item) => {
+    console.log(item, 'itemitem')
     msgs.value = msgs.value + item
+}
+const inputDelete = (item) => {
+    msgs.value = msgs.value.slice(0, -1)
 }
 </script>
 <template>
-    <div>{{ msgs }}</div>
-    <Keyboard @inputKey="inputKey" />
+    <div style="display: flex; justify-content: center; margin-top: 20px">
+        <input type="text" style="width: 90%" v-model="msgs" />
+    </div>
+    <Keyboard @inputKey="inputKey" @inputDelete="inputDelete" />
     <!-- <FalseTextbox /> -->
 </template>
 <style scoped></style>
--- packages/keyboard/src/keyboard.vue
+++ packages/keyboard/src/keyboard.vue
@@ -15,15 +15,22 @@ onMounted(() => {})
 const keyEnInput1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
 const keyEnInput2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
 const keyEnInput3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
-const touchstartButton = (e) => {
-    e.target.style.background = `#acb0bb`
+const touchstartButton = (e, type) => {
+    console.log(type, e.target, 'typetype')
+
+    if (type === 'delete') {
+        e.target.style.background = `#ffffff`
+    } else {
+        e.target.style.background = `#acb0bb`
+    }
 }
 const touchendButton = (e, type, item) => {
-    e.target.style.background = `#ffffff`
     if (type === 'delete') {
         emit('inputDelete')
+        e.target.style.background = `#acb0bb`
     } else {
         emit('inputKey', item)
+        e.target.style.background = `#ffffff`
     }
 }
 </script>
@@ -34,7 +41,8 @@ const touchendButton = (e, type, item) => {
                 class="keyboard-item__keyboardhmx button__boxshadow"
                 v-for="item in keyEnInput1"
                 @touchstart="touchstartButton"
-                @touchend="touchendButton($event, 'input', item)">
+                @touchend="touchendButton($event, 'input', item)"
+            >
                 {{ item }}
             </div>
         </div>
@@ -43,13 +51,15 @@ const touchendButton = (e, type, item) => {
                 class="keyboard-item__keyboardhmx button__boxshadow"
                 v-for="item in keyEnInput2"
                 @touchstart="touchstartButton"
-                @touchend="touchendButton($event, 'input', item)">
+                @touchend="touchendButton($event, 'input', item)"
+            >
                 {{ item }}
             </div>
         </div>
         <div class="keyboard-line__keyboardhmx">
             <div
-                class="operation-button__keyboardhmx peration-button-left__keyboardhmx button__boxshadow">
+                class="operation-button__keyboardhmx peration-button-left__keyboardhmx button__boxshadow"
+            >
                 <svg
                     t="1676970939334"
                     class="icon"
@@ -58,24 +68,28 @@ const touchendButton = (e, type, item) => {
                     xmlns="http://www.w3.org/2000/svg"
                     p-id="4062"
                     width="32"
-                    height="32">
+                    height="32"
+                >
                     <path
                         d="M688.64 995.2H341.12a53.12 53.12 0 0 1-53.12-52.48V520.96H45.44a32 32 0 0 1-30.08-21.12 30.72 30.72 0 0 1 8.32-35.2L512 21.76a32 32 0 0 1 44.16 0l442.88 442.24a33.92 33.92 0 0 1 7.04 35.84 32 32 0 0 1-30.08 19.84h-215.68v402.56a71.68 71.68 0 0 1-71.68 72.96z m-336-64h336a7.04 7.04 0 0 0 7.04-7.04V488.32a32 32 0 0 1 32.64-32h172.16L535.04 90.88 128 456.32h192a32 32 0 0 1 32.64 32z"
                         fill="#323333"
-                        p-id="4063"></path>
+                        p-id="4063"
+                    ></path>
                 </svg>
             </div>
             <div
                 class="keyboard-item__keyboardhmx button__boxshadow"
                 v-for="item in keyEnInput3"
                 @touchstart="touchstartButton"
-                @touchend="touchendButton($event, 'input', item)">
+                @touchend="touchendButton($event, 'input', item)"
+            >
                 {{ item }}
             </div>
             <div
                 class="operation-button__keyboardhmx peration-button-right__keyboardhmx button__boxshadow"
-                @touchstart="touchstartButton"
-                @touchend="touchendButton($event, 'delete', '')">
+                @touchstart.prevent="touchstartButton($event, 'delete')"
+                @touchend.prevent="touchendButton($event, 'delete', '')"
+            >
                 <svg
                     t="1676971082940"
                     class="icon"
@@ -84,10 +98,12 @@ const touchendButton = (e, type, item) => {
                     xmlns="http://www.w3.org/2000/svg"
                     p-id="4216"
                     width="32"
-                    height="32">
+                    height="32"
+                >
                     <path
                         d="M856.2 890.5H402.4c-55.3-0.1-108-23.4-145.4-64.2L23 571c-30.7-33.4-30.7-84.8 0-118.2l233.9-255.1c37.3-40.8 90.1-64.1 145.4-64.2h453.8c92.7 0.2 167.8 75.4 167.8 168.1v420.7c0.1 92.8-75 168-167.7 168.2zM402.6 184.9c-41 0-80 17.2-107.8 47.4L60.9 487.6c-12.8 13.7-12.8 34.9 0 48.6l233.7 255.3c27.7 30.2 66.8 47.4 107.8 47.4h454c64.4-0.1 116.5-52.4 116.5-116.8V301.6c0-64.4-52.2-116.6-116.5-116.8H402.6z m323.7 471c-6.8 0-13.4-2.7-18.2-7.5l-100-100.2L508 648.4c-9.9 10.1-26.1 10.2-36.2 0.3-10.1-9.9-10.2-26.1-0.3-36.2l0.4-0.4 100-100.2-100-100.2c-10.1-9.9-10.3-26.1-0.4-36.2 9.9-10.1 26.1-10.3 36.2-0.4l0.4 0.4 100 100.2 100-100.2c10-10 26.2-10 36.2 0 10 10 10 26.2 0 36.2l-100 100.2 100.2 100.2c10 10 10 26.3-0.1 36.3-4.8 4.8-11.3 7.5-18.1 7.5z"
-                        p-id="4217"></path>
+                        p-id="4217"
+                    ></path>
                 </svg>
             </div>
         </div>
@@ -96,7 +112,8 @@ const touchendButton = (e, type, item) => {
             <div
                 class="space__keyboardhmx button__boxshadow"
                 @touchstart="touchstartButton"
-                @touchend="touchendButton($event, 'input', `&nbsp;`)">
+                @touchend="touchendButton($event, 'input', `&nbsp;`)"
+            >
                 space
             </div>
             <div class="ok__keyboardhmx button__boxshadow">ok</div>
@@ -150,6 +167,8 @@ const touchendButton = (e, type, item) => {
 }
 .peration-button-right__keyboardhmx .icon {
     width: 6vw;
+    background-color: transparent !important;
+    pointer-events: none;
 }
 .switch__keyboardhmx {
     width: 23vw;
